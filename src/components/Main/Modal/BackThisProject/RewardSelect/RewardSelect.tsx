650d3f8644244bee785ca39faf120d87c7c9e708
Huy Phan <huy.phan2210@outlook.com>
2023-04-07 16:23:08 +0700

    Update RewardSelect Component

--- src/components/Main/Modal/BackThisProject/RewardSelect/RewardSelect.tsx
+++ src/components/Main/Modal/BackThisProject/RewardSelect/RewardSelect.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from "react";
+import { MouseEvent, KeyboardEvent, ChangeEvent, useState } from "react";
 
 import { RewardDto } from "../../../../../dto/reward.dto";
 
@@ -7,13 +7,66 @@ import './RewardSelect.css';
 import CustomCheckbox from "./CustomCheckbox/CustomCheckbox";
 
 function RewardSelect(props: RewardDto) { 
+    const [pledge, setPledge] = useState(0);
+
     function hanldeSelectReward(e: MouseEvent<HTMLDivElement>) {
+        if (props.stockNumber === 0) {
+            e.preventDefault();
+            return;
+        }
+
+        // Outline Handler
+        if (e.currentTarget.parentElement) {
+            const rewardSelections = document.getElementsByClassName('reward-select');
+            for (let i = 0; i < rewardSelections.length; i++) {
+                rewardSelections[i].setAttribute('style', '');
+                rewardSelections[i].getElementsByClassName('proceed')[0].setAttribute('style', '');
+            }
+
+            e.currentTarget.parentElement.style.outlineColor = 'var(--moderate-cyan)';
+            e.currentTarget.parentElement.style.outlineWidth = '0.2rem';
+            e.currentTarget.parentElement.style.outlineStyle = 'solid';
+
+            // Handling content justification of proceed class 
+            const aspectRatio = window.innerWidth / window.innerHeight;
+            if (document.getElementsByClassName('proceed')[0] === e.currentTarget.parentElement.getElementsByClassName('proceed')[0]) {
+                if (aspectRatio >= 1) {
+                    document.getElementsByClassName('proceed')[0].setAttribute('style', 'display: flex; justify-content: end;');
+                } else {
+                    document.getElementsByClassName('proceed')[0].setAttribute('style', 'display: flex; justify-content: center;');
+                }
+                
+            } else {
+                if (aspectRatio >= 1) {
+                    e.currentTarget.parentElement.getElementsByClassName('proceed')[0].setAttribute('style', 'display: flex;');
+                } else {
+                    e.currentTarget.parentElement.getElementsByClassName('proceed')[0].setAttribute('style', 'display: block;');
+                }
+                
+            }
+        }
+        
+
+        // Checkbox Handler
         const checkboxes = document.getElementsByClassName('checkbox');
         for (let i = 0; i < checkboxes.length; i++) {
             checkboxes[i].getElementsByClassName('checked')[0].setAttribute('style', '');
         }
-
         e.currentTarget.getElementsByClassName('checked')[0].setAttribute('style', 'display: block;');
+    }
+
+    function preventInvalidLetters(e: KeyboardEvent<HTMLInputElement>, regexr: RegExp) {
+        if (!regexr.test(e.key) && e.key !== 'Tab' && e.key !== 'Backspace' && e.key !== 'Delete' && e.key !== 'F5') {
+            e.preventDefault();
+        }    
+    }
+
+    function handlePledge(e: ChangeEvent<HTMLInputElement>) {
+        if (!isNaN(parseInt(e.currentTarget.value))) {
+            setPledge(parseInt(e.currentTarget.value));
+        } else {
+            setPledge(0);
+        }
         
     }
     return (
@@ -30,7 +83,13 @@ function RewardSelect(props: RewardDto) {
                 <p>{props.description}</p>
                 {props.stockNumber || props.stockNumber === 0 ? <h3 className="stock-mobile">{props.stockNumber} <span>left</span></h3> : ''}
             </div>
-            
+            <div className="proceed">
+                {props.pledgeNumber ? <p>Enter your pledge</p> : ''}
+                <div className="proceed-actions">
+                    {props.pledgeNumber ? <input type="text" placeholder={String(props.pledgeNumber)} onKeyDown={(e) => preventInvalidLetters(e, /^[0-9]+$/)} onChange={handlePledge}></input> : ''}
+                    <button type="button" disabled={props.pledgeNumber ? pledge < props.pledgeNumber : !props.pledgeNumber ? false : true} onClick={props.handlePledge}>Continue</button>
+                </div>
+            </div>
         </div>
     )
 }
