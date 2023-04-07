4793dd6e81f18fc112ee94c7ccb106c82fd312c6
Huy Phan <huy.phan2210@outlook.com>
2023-04-07 16:23:37 +0700

    Update MainContent.tsx

--- src/components/Main/MainContent.tsx
+++ src/components/Main/MainContent.tsx
@@ -1,4 +1,4 @@
-import { useState, MouseEvent } from 'react';
+import { useState, useEffect, MouseEvent } from 'react';
 
 import rewardData from '../../data/rewards.json'
 
@@ -20,12 +20,24 @@ function MainContent() {
   }
 
   function turnOnModal() {
+    const heading = document.getElementById('heading');
+    const backThisProjectElement = document.getElementById('back-this-project');
+    const thankYouElement = document.getElementById('thank-you');
     const overlay = document.getElementById('overlay');
     const modal = document.getElementById('modal');
+    if (backThisProjectElement && thankYouElement && heading) {
+      backThisProjectElement.style.display = '';
+      thankYouElement.style.display = '';
+      heading.style.zIndex = '0';
+    }
     if (overlay) {
       overlay.style.display = 'block';
     }
     if (modal) {
+      modal.style.position = '';
+      modal.style.width = '';
+      modal.style.top = '';
+      modal.style.transform = '';
       modal.style.display = 'block';
     }
   }
@@ -43,6 +55,44 @@ function MainContent() {
       e.currentTarget.getElementsByTagName('img')[1].style.display = '';
     }
   }
+
+  function handlePledge(e: MouseEvent<HTMLButtonElement>) {
+    const backThisProjectElement = document.getElementById('back-this-project');
+    const thankYouElement = document.getElementById('thank-you');
+    const modal = document.getElementById('modal');
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    if (backThisProjectElement && thankYouElement && modal) {
+      backThisProjectElement.style.display = 'none';
+      thankYouElement.style.display = 'block';
+      modal.style.position = 'fixed';
+      modal.style.top = '50%';
+      modal.style.transform = 'translate(-50%, -50%)';
+      if (aspectRatio > 1) {
+        modal.style.width = 'calc(20% - 4rem)';
+      } else if (aspectRatio > 0.5) {
+        modal.style.width = 'calc(50% - 4rem)';
+      } else {
+        modal.style.width = 'calc(90% - 4rem)';
+      }
+    }
+    
+    if (e.currentTarget.parentElement) {
+      const pledgeInput = e.currentTarget.parentElement.getElementsByTagName('input')[0];
+      if (pledgeInput) {
+        const newRewards = [...rewardData.reward];
+        if (parseInt(pledgeInput.placeholder) === 25) {
+          newRewards[0].stockNumber--;
+        } else if (parseInt(pledgeInput.placeholder) === 75) {
+          newRewards[1].stockNumber--;
+        }
+        setRewards([...newRewards])
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.getElementsByClassName('checkbox')[0].setAttribute('style', 'transform: none;')
+  }, [])
   return (
     <div id='content'>
       <section id='intro'>
@@ -92,7 +142,7 @@ function MainContent() {
         {rewards.map((reward, i) => <Reward key={i} heading={reward.heading} description={reward.description} pledgeNumber={reward.pledgeNumber} stockNumber={reward.stockNumber}></Reward>)}
       </section>
       <div id='overlay'></div>
-      <Modal rewards={[noReward, ...rewards]}></Modal>
+      <Modal rewards={[noReward, ...rewards]} handlePledge={handlePledge}></Modal>
   </div>
   )
 }
