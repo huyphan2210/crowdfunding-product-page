8e571690bbc05c31cc5d192b7370a36ec6f6d9f7
Huy Phan <huy.phan2210@outlook.com>
2023-04-07 16:23:32 +0700

    Update BackThisProject Component

--- src/components/Main/Modal/BackThisProject/BackThisProject.tsx
+++ src/components/Main/Modal/BackThisProject/BackThisProject.tsx
@@ -1,16 +1,18 @@
 import './BackThisProject.css';
 import { RewardDto } from "../../../../dto/reward.dto";
 import RewardSelect from './RewardSelect/RewardSelect';
+import { MouseEventHandler } from 'react';
 
 
 function BackThisProject(props: {
-    rewards: RewardDto[]
+    rewards: RewardDto[],
+    handlePledge: MouseEventHandler
 }) {
     return (
         <div id='back-this-project'>
              <h2>Back this project</h2>
              <p>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
-             {props.rewards.map((reward, i) => <RewardSelect key={i} heading={reward.heading} description={reward.description} pledgeNumber={reward.pledgeNumber} stockNumber={reward.stockNumber}></RewardSelect>)}
+             {props.rewards.map((reward, i) => <RewardSelect handlePledge={props.handlePledge} key={i} heading={reward.heading} description={reward.description} pledgeNumber={reward.pledgeNumber} stockNumber={reward.stockNumber}></RewardSelect>)}
         </div>
     )
 }
